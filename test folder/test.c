621098817026e89ec0457efdc9f0ad4b798a9588
s0fia090 <sofia.beltran@ucas-edu.net>
2024-09-11 03:08:27 +0000

    changed testc code

--- test folder/test.c
+++ test folder/test.c
@@ -1,25 +1,6 @@
 #include <stdio.h>
 
-int main(void) {
-char name[]= "Sofia Beltran";
-printf("%s\n", name);
-char numbermini[]= "7";
-printf("%s\n", numbermini);
-char numberbig[]= "690";
-printf("%s\n", numberbig);
-char breakfast[]= "ego's";
-printf("%s\n", breakfast);
-char favoritecolor[]="red";
-printf("%s\n", favoritecolor);
-char school[]="UCAS";
-printf("%s\n", school);
-char year[]= "2024";
-printf("%s\n", year);
-char eyecolor[]= "brown";
-printf("%s\n", eyecolor);
-char age[]= "14";
-printf("%s\n", )
-
+int main(void){
+    printf("Hello World");
     return 0;
-
 }
--- variables/practice.c
+++ variables/practice.c
@@ -1,21 +1,28 @@
-name=(sofia);
+#include <stdio.h>
 
-nmbr110=(7);
+int main(void) {
+char name[]= "Sofia Beltran";
+printf("%s\n", name);
+char numbermini[]= "7";
+printf("%s\n", numbermini);
+char numberbig[]= "690";
+printf("%s\n", numberbig);
+char breakfast[]= "ego's";
+printf("%s\n", breakfast);
+char favoritecolor[]="red";
+printf("%s\n", favoritecolor);
+char school[]="UCAS";
+printf("%s\n", school);
+char year[]= "2024";
+printf("%s\n", year);
+char eyecolor[]= "brown";
+printf("%s\n", eyecolor);
+char age[]= "14";
+printf("%s\n", age);
+char subject[]= "geography";
+printf("%s\n", subject);
 
-nmbr1700= ("655")
 
-bkrfst=(egos);
+    return 0;
 
-favcol= (red);
-
-schl= (UCAS);
-
-yr=(2024);
-
-eyecol="brown
-
-age=14;
-
-favsubgeography
-
-print="name nmbr110 brkfst favcol schl yr eyecol age favsub"
+}
