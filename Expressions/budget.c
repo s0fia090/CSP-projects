34df8d5694cbd79ccc198d8783122d3b598253d0
s0fia090 <sofia.beltran@ucas-edu.net>
2024-09-16 19:26:29 +0000

    finished and fixed budget.c

--- Expressions/budget.c
+++ Expressions/budget.c
@@ -1,16 +1,36 @@
 #include <stdio.h>
 
 int main(void){
-    char income[9];
-    char utilities[9];
-    char groceries[9];
-    char car[9];
+    float income, rent, utilities, groceries, transportation, savings, expenses, spend;
+    float prent, putilities, pgroceries, ptransportation, psavings, pexpenses, pspend;
     printf("This is a budget calculator.\n How much do you make a month?\n");
-    scanf("%s", income);
+    scanf("%f", &income);
+    printf("how much do your rent cost?\n");
+    scanf("%f", &rent);
     printf("how much do your utilities cost?\n");
-    scanf("%s", utilities);
-    printf("You make %s\n", income");
-    float spend[] = (float) income - (float) utilities;
-    printf("%f",spend);
+    scanf("%f", &utilities);
+    printf("how much do your groceries cost?\n");
+    scanf("%f", &groceries);
+    printf("how much do your transportation cost?\n");
+    scanf("%f", &transportation);
+    savings = income * .2;
+    expenses = rent + utilities + groceries + transportation;
+    spend = income - expenses - savings;
+    printf("You make $%.2f\n", income);
+    printf("your expenses are $%.2f\n", expenses);
+    printf("your savings are $%.2f\n", savings);
+    printf("your spending money is $%.2f\n", spend);
+    prent = rent/income *100;
+    putilities = utilities/income *100;
+    pgroceries = groceries/income *100;
+    ptransportation = transportation/income *100;
+    psavings = savings/income *100;
+    pexpenses = expenses/income *100;
+    printf("your rent is %d%% of your income \n", (int) prent);
+    printf("your utilities is %d%% of your income \n", (int) putilities);
+    printf("your groceries is %d%% of your income \n", (int) pgroceries);
+    printf("your transportation is %d%% of your income \n", (int) ptransportation);
+    printf("your savings is %d%% of your income \n", (int) psavings);
+    printf("your expenses is %d%% of your income \n", (int) pexpenses);
     return 0;
 }
