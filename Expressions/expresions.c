7682ce22c07e84a015488d57a3120b4288871e89
s0fia090 <sofia.beltran@ucas-edu.net>
2024-09-18 04:58:12 +0000

    finished expressions c and changed variable names

---
+++ Expressions/expresions.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void){
+     int a = 7-24/8*4+6;
+    printf("%d\n",a);
+    int b = 18/3-7+2*5;
+    printf("%d\n",b);
+    int c = 6*4/12+72/8-9;
+    printf("%d\n",c);
+    int d = ((17-6/2)+4*3);
+    printf("%d\n",d);
+    int e = -2*(1*4-2/2)+(6+2-3);
+    printf("%d\n",e);
+    int f = -1*((3-4*7)/5)-2*24/6;
+    printf("%d\n",f);
+    int g = (3*pow(5,2)/15)-(5-pow(2,2));
+    printf("%d\n",g);
+    int h = (pow(1,4)*pow(2,2)+pow(3,3))-(pow(2,5)/4);
+    printf("%d\n",h);
+    int i = (pow(22/2-2*5,2)+pow(4-6/6,2));
+    printf("%d\n",i);
+    return 0;
+}
