32d299ea4209b67e931f7eb5070896fe64245c6e
s0fia090 <sofia.beltran@ucas-edu.net>
2024-09-20 19:19:55 +0000

    made silly semntences in py

---
+++ strings/sillysentences.py
@@ -0,0 +1,7 @@
+print("name an animal")
+animal = input()
+print("name a place")
+place = input()
+print("name a past tense verb")
+verb = input()
+print("the", animal + " went to the", place + " and", verb + " with his friend the angry turtle")
