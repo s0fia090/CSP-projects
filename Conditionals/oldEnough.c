6536bd8b61b8e3a0ca105df4b1d240597250c316
s0fia090 <sofia.beltran@ucas-edu.net>
2024-10-03 19:10:44 +0000

    added stuff

--- Conditionals/oldEnough.c
+++ Conditionals/oldEnough.c
@@ -1,13 +1,26 @@
 #include <stdio.h>
-#include <stdbool
 
-int age = 20;
-it main (void){
-    printf("how old are you?")
-    if(age >= 18){
-        printf("you are old enough to vote!");
-    }else if (age =< 18){
-        printf("you are not old enought to vote");
-    };
-    return 0:
+int age;
+
+int main (void){
+    printf("how old are you?\n");
+    scanf("%d", &age);
+    if (age >= 18){
+        printf("you are old enough to vote!\n");
+    }else if (age <= 18){
+        printf("you are not old enought to vote\n");
+    }if (age >= 16){
+        printf("you are old enoughto drive!\n");
+    }else if (age <= 16){
+        printf("you are not old enough to drive\n");
+    }if (age >= 16){
+        printf("your are able to get your learners permit");
+    }else if (age <= 16){
+        printf("you are  not old enough to have your learners permit\n");
+    }if (age >= 5){
+        printf("you are old enough to go to school\n");
+    }else if (age <= 5){
+        printf("you are not old enough to go to school!\n");
+    }
+    return 0;
 }
--- Conditionals/timeOfDay.c
+++ Conditionals/timeOfDay.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 #include <time.h>
 
+time_t now;
+struct tm *timeinfo;
+
 int hour;
 
 int main ()
 {
     time_t rawtime;
-    struct tm *timeinfo;
+    struct tm *now_tm;
 
+    now = time(NULL);
+    now_tm = localtime(&now);
+    hour = (now_tm->tm_hour)-6;
     time(&rawtime);
     timeinfo = localtime (&rawtime);
-    printf("Current local time and date: %s", asctime(timeinfo));
+    printf("surrent local time and date: %s", asctime(timeinfo));
     printf("tell me the hour in military time\n");
     scanf("%d", &hour);
     if(hour < 12){
