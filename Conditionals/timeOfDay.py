7128b3e6aa6816949756748fa1d1f40f31bd0d60
s0fia090 <sofia.beltran@ucas-edu.net>
2024-10-03 19:37:18 +0000

    made time inpy

--- Conditionals/timeOfDay.py
+++ Conditionals/timeOfDay.py
@@ -3,3 +3,9 @@ import datetime
 currenttime = datetime.datetime.now()
 
 print(currenttime.hour)
+if currenttime.hour <=12:
+    print("good morning")
+elif currenttime.hour <18:
+    print("good afternoon")
+else:
+    print("good evening")
