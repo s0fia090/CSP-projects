2ababd614caacb22a1252b7f82b7fcff79120a17
s0fia090 <sofia.beltran@ucas-edu.net>
2024-10-03 19:20:03 +0000

    fixed old enough in py

--- Conditionals/oldEnough.py
+++ Conditionals/oldEnough.py
@@ -3,11 +3,3 @@ if age >= 18:
     print("you are old enough to vote!")
 elif age <18:
     print("you are not old enough to vote:(")
-elif age <16:
-    print("you are not old enough to drive:(")
-elif age <15:
-    print("you are not old enough to get your learners permit:(")
-elif age <5:
-    print("you are not old enough to go to school:(")
-else:
-    print("you are too young to go to school")
