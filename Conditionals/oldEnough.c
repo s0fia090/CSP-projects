6b136e5602869803fcf9b32963c3393cac8cbdd0
s0fia090 <sofia.beltran@ucas-edu.net>
2024-10-03 19:18:53 +0000

    made old enough in c

--- Conditionals/oldEnough.c
+++ Conditionals/oldEnough.c
@@ -7,20 +7,8 @@ int main (void){
     scanf("%d", &age);
     if (age >= 18){
         printf("you are old enough to vote!\n");
-    }else if (age <= 18){
+    }else{
         printf("you are not old enought to vote\n");
-    }if (age >= 16){
-        printf("you are old enoughto drive!\n");
-    }else if (age <= 16){
-        printf("you are not old enough to drive\n");
-    }if (age >= 16){
-        printf("your are able to get your learners permit");
-    }else if (age <= 16){
-        printf("you are  not old enough to have your learners permit\n");
-    }if (age >= 5){
-        printf("you are old enough to go to school\n");
-    }else if (age <= 5){
-        printf("you are not old enough to go to school!\n");
     }
     return 0;
 }
