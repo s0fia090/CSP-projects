dc2c3e1600acca8e37dae0531bd5f0f1b97953b9
s0fia090 <sofia.beltran@ucas-edu.net>
2024-10-04 02:14:34 +0000

    fixed old enough in c

--- Conditionals/oldEnough.c
+++ Conditionals/oldEnough.c
@@ -7,9 +7,12 @@ int main (void){
     scanf("%d", &age);
     if (age >= 18){
         printf("you are old enough to vote!\n");
-    }else if(age <= 16)
-        
-
+    }else if(age >= 16){
+        printf("you are old enough to drive\n");
+    }else if(age >= 16){
+        printf("you are old to get learners permit\n");
+    }else if(age >= 5){
+        printf("you are old enough to go to school\n");
     }
     return 0;
 }
