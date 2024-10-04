193d153cf27f5ffc69356bab502cda71363c8a81
s0fia090 <sofia.beltran@ucas-edu.net>
2024-10-04 19:15:39 +0000

    made py practice var

---
+++ variables/practice.py
@@ -0,0 +1,20 @@
+name = "sofia"
+print(name)
+numsmall = "7"
+print(numsmall)
+numbig = "7000"
+print(numbig)
+brkfst = "eggsandwich"
+print(brkfst)
+col = "darkred"
+print(col)
+schlyer = "2024-25"
+print(schlyer)
+yer = "2024"
+print(yer)
+eyecol = "darkbrown"
+print(eyecol)
+age = "14"
+print(age)
+favsub = "drawing"
+print(favsub)
